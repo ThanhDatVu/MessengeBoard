c065e206d6e16622be82b75f0f640e3aa5730095
TienTienBear <dat2611gd@gmail.com>
2022-05-17 16:51:32 +0700

    final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
-
+var messages = [
+  {
+    title: 'Hello',
+    text: "Hi there!",
+    username: "Amando",
+    date: new Date()
+  },
+  
+  {
+    title: 'Hello',
+    text: "Hello World!",
+    username: "Charles",
+    date: new Date()
+  }
+];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
-
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: "Mini Messageboard" });
+});
+router.post('/new', function(req, res, next) {
+  messages.push(  {
+    title: req.body.title,
+    text: req.body.text,
+    username: req.body.username,
+    date: new Date()
+  });
+  
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
 module.exports = router;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./views/*.{pug,md,html}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
